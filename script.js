987d51ac53f1f52693ba8af01238080a3cc4576f
Marwan Samaha <113918967+MarwanSaMaha12@users.noreply.github.com>
2024-08-20 01:09:37 +0300

    Upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "UI/UX Desginer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
+
+let main = document.querySelector(".main")
+let toggler = document.querySelector("nav")
+
+toggler.addEventListener('clicked', function(){
+    main.remove
+    console.log("hi")
+})
